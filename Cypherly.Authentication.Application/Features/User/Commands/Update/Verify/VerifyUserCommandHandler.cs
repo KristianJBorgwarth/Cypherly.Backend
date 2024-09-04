a82499393450c6b074c93cbc974cc05044dd9764
Kristian <fich2402@gmail.com>
2024-09-04 11:33:19 +0200

    added cmd, validator and handler

---
+++ Cypherly.Authentication.Application/Features/User/Commands/Update/Verify/VerifyUserCommandHandler.cs
@@ -0,0 +1,38 @@
+﻿using Cypherly.Application.Abstractions;
+using Cypherly.Application.Contracts.Repository;
+using Cypherly.Authentication.Application.Contracts;
+using Cypherly.Domain.Common;
+using Microsoft.Extensions.Logging;
+
+namespace Cypherly.Authentication.Application.Features.User.Commands.Update.Verify;
+
+public class VerifyUserCommandHandler(
+    ILogger<VerifyUserCommandHandler> logger,
+    IUserRepository userRepository,
+    IUnitOfWork unitOfWork)
+    : ICommandHandler<VerifyUserCommand, Result>
+{
+    public async Task<Result<Result>> Handle(VerifyUserCommand request, CancellationToken cancellationToken)
+    {
+        try
+        {
+            var user = await userRepository.GetByIdAsync(request.UserId);
+            if (user is null)
+                return Result.Fail(Errors.General.NotFound(request.UserId));
+            
+            var result = user.Verify(request.VerificationCode);
+            if (result.Success is false)
+                return Result.Fail(result.Error);
+
+            await userRepository.UpdateAsync(user);
+            await unitOfWork.SaveChangesAsync(cancellationToken);
+
+            return Result.Ok();
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "Error handling VerifyUserCommand");
+            return Result.Fail(Errors.General.UnspecifiedError("Exception occured attempting to verify the user. Check logs for more information"));
+        }
+    }
+}
---
+++ Cypherly.Authentication.Application/Features/User/Commands/Update/Verify/VerifyUserCommandValidator.cs
@@ -0,0 +1,11 @@
+﻿using FluentValidation;
+
+namespace Cypherly.Authentication.Application.Features.User.Commands.Update.Verify;
+
+public class VerifyUserCommandValidator : AbstractValidator<VerifyUserCommand>
+{
+    public VerifyUserCommandValidator()
+    {
+     
+    }
+}
