a332a70e5135b9bbfdff8ddd3d25df2aa48d1ef3
Kristian <fich2402@gmail.com>
2024-09-06 09:21:36 +0200

    Email Service and configuration added to MiniEmail.API

---
+++ MiniEmail.API/Configuration/SmtpClientConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using MailKit.Net.Smtp;
+using MailKit.Security;
+using Microsoft.Extensions.Options;
+
+namespace MinimalEmail.API.Configuration;
+
+public static class SmtpClientConfiguration
+{
+    public static IServiceCollection AddSmtpClient(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.Configure<SmtpSettings>(configuration.GetSection("SmtpSettings"));
+
+        services.AddSingleton<SmtpClient>(s =>
+        {
+            var smtpSettings = s.GetRequiredService<IOptions<SmtpSettings>>().Value;
+            var smtpClient = new SmtpClient();
+            smtpClient.Connect(smtpSettings.Host, smtpSettings.Port, SecureSocketOptions.StartTls);
+            smtpClient.Authenticate(smtpSettings.Username, smtpSettings.Password);
+
+            return smtpClient;
+        });
+
+        return services;
+    }
+}
--- MiniEmail.API/Features/Email/EmailService.cs
+++ MiniEmail.API/Features/Email/EmailService.cs
@@ -1,11 +1,39 @@
 ﻿using FluentResults;
+using MailKit.Net.Smtp;
+using MimeKit;
 
 namespace MinimalEmail.API.Features.Email;
 
-public class EmailService : IEmailService
+public class EmailService(SmtpClient smtpClient, ILogger<EmailService> logger)
+    : IEmailService
 {
-    public Task<Result> SendEmailAsync(string to, string subject, string body)
+    private const string FromAddress = "noreply@cypherl.org";
+    private const string SenderName = "Cypherly";
+
+    public async Task<Result> SendEmailAsync(string to, string subject, string body)
     {
-        throw new NotImplementedException();
+        try
+        {
+            var message = new MimeMessage();
+
+            message.From.Add(new MailboxAddress(SenderName, FromAddress));
+
+            message.To.Add(new MailboxAddress(null, to));
+
+            message.Subject = subject;
+
+            message.Body = new TextPart("html")
+            {
+                Text = body
+            };
+
+            await smtpClient.SendAsync(message);
+            return Result.Ok();
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "Failed to send email");
+            return Result.Fail(ex.Message);
+        }
     }
 }
