9e3db21c9e0b31f3c73d91554d4067c731094473
Kristian <89026749+KristianJBorgwarth@users.noreply.github.com>
2024-09-17 16:34:10 +0200

    Delete Friendship Feature (#14)

---
+++ Cypherly.UserManagement.Application/Features/UserProfile/Commands/Delete/Friendship/DeleteFriendshipCommandValidator.cs
@@ -0,0 +1,22 @@
+﻿using Cypherly.Domain.Common;
+using FluentValidation;
+
+namespace Cypherly.UserManagement.Application.Features.UserProfile.Commands.Delete.Friendship;
+
+public class DeleteFriendshipCommandValidator : AbstractValidator<DeleteFriendshipCommand>
+{
+    public DeleteFriendshipCommandValidator()
+    {
+        RuleLevelCascadeMode = CascadeMode.Stop;
+        RuleFor(x=> x.UserProfileId)
+            .NotNull().WithMessage(Errors.General.ValueIsRequired(nameof(DeleteFriendshipCommand.UserProfileId)).Message)
+            .NotEmpty().WithMessage(Errors.General.ValueIsEmpty(nameof(DeleteFriendshipCommand.UserProfileId)).Message);
+
+        RuleFor(x => x.FriendTag)
+            .NotNull().WithMessage(Errors.General.ValueIsRequired(nameof(DeleteFriendshipCommand.FriendTag)).Message)
+            .NotEmpty().WithMessage(Errors.General.ValueIsEmpty(nameof(DeleteFriendshipCommand.FriendTag)).Message)
+            .Must(x => x.Length <= 20)
+            .WithMessage(Errors.General.ValueTooLarge(nameof(DeleteFriendshipCommand.FriendTag), 20).Message);
+        
+    }
+}
