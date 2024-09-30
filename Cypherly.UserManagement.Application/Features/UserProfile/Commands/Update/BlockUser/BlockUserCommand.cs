e8968a4a0b1522912d734842627bde25a4b2901b
Kristian <fich2402@gmail.com>
2024-09-30 06:06:56 +0200

    setup: blockusercommand, -handler, -validator

---
+++ Cypherly.UserManagement.Application/Features/UserProfile/Commands/Update/BlockUser/BlockUserCommand.cs
@@ -0,0 +1,9 @@
+﻿using Cypherly.Application.Abstractions;
+
+namespace Cypherly.UserManagement.Application.Features.UserProfile.Commands.Update.BlockUser;
+
+public sealed record BlockUserCommand : ICommand
+{
+    public Guid UserId { get; init; }
+    public required string BlockedUserTag { get; init; }
+}
---
+++ Cypherly.UserManagement.Application/Features/UserProfile/Commands/Update/BlockUser/BlockUserCommandHandler.cs
@@ -0,0 +1,21 @@
+﻿using Cypherly.Application.Abstractions;
+using Cypherly.Application.Contracts.Repository;
+using Cypherly.Domain.Common;
+using Cypherly.UserManagement.Application.Contracts;
+using Cypherly.UserManagement.Domain.Services;
+using Microsoft.Extensions.Logging;
+
+namespace Cypherly.UserManagement.Application.Features.UserProfile.Commands.Update.BlockUser;
+
+public class BlockUserCommandHandler(
+    IUserProfileRepository profileRepository,
+    IUserProfileService profileService,
+    IUnitOfWork uow,
+    ILogger<BlockUserCommandHandler> logger)
+    : ICommandHandler<BlockUserCommand>
+{
+    public Task<Result> Handle(BlockUserCommand request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
