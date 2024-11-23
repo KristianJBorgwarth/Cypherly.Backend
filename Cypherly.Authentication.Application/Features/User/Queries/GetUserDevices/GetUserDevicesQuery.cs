aea9f7c34eb6b61c2b584b99eef60142cec55c0e
Lasse Schwenger <lasse.m.schwenger@gmail.com>
2024-11-23 23:10:33 +0100

    added option to fetch trusted devices

---
+++ Cypherly.Authentication.Application/Features/User/Queries/GetUserDevices/GetUserDevicesQuery.cs
@@ -0,0 +1,9 @@
+﻿using Cypherly.Application.Abstractions;
+using Cypherly.Authentication.Application.Features.User.Queries.GetUserDevices;
+
+namespace Cypherly.Authentication.Application.Features.User.Queries.GetUserDevices;
+
+public sealed record GetUserDevicesQuery : IQuery<GetUserDevicesDto>
+{
+    public required Guid UserId { get; init; }
+}
