7792caf7494448e81056aa072a7061a81fb24522
Lasse Schwenger <lasse.m.schwenger@gmail.com>
2024-11-23 23:56:44 +0100

    Added unittests for handler and validator

---
+++ Cypherly.Authentication.Application/Features/User/Queries/GetUserDevices/GetUserDevicesQueryValidator.cs
@@ -0,0 +1,13 @@
+﻿using Cypherly.Domain.Common;
+using FluentValidation;
+
+namespace Cypherly.Authentication.Application.Features.User.Queries.GetUserDevices;
+
+public class GetUserDevicesQueryValidator : AbstractValidator<GetUserDevicesQuery>
+{
+    public GetUserDevicesQueryValidator()
+    {
+        RuleFor(x => x.UserId)
+            .NotEmpty().WithMessage(Errors.General.ValueIsEmpty(nameof(GetUserDevicesQuery.UserId)).Message);
+    }
+}
