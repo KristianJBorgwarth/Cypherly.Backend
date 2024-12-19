ed4b6255857f6342f77a71134f0f058425c326a2
Kristian <fich2402@gmail.com>
2024-12-19 01:29:45 +0100

    implemented VerifyNonceCommandValidator

--- Cypherly.Authentication.Application/Features/Authentication/Commands/VerifyNonce/VerifyNonceCommandValidator.cs
+++ Cypherly.Authentication.Application/Features/Authentication/Commands/VerifyNonce/VerifyNonceCommandValidator.cs
@@ -1,4 +1,5 @@
-﻿using FluentValidation;
+﻿using Cypherly.Domain.Common;
+using FluentValidation;
 
 namespace Cypherly.Authentication.Application.Features.Authentication.Commands.VerifyNonce;
 
@@ -6,6 +7,15 @@ public class VerifyNonceCommandValidator : AbstractValidator<VerifyNonceCommand>
 {
     public VerifyNonceCommandValidator()
     {
-
+        RuleFor(x=> x.UserId)
+            .NotEmpty().WithMessage(Errors.General.ValueIsEmpty(nameof(VerifyNonceCommand.UserId)).Message);
+        RuleFor(x=> x.NonceId)
+            .NotEmpty().WithMessage(Errors.General.ValueIsEmpty(nameof(VerifyNonceCommand.NonceId)).Message);
+        RuleFor(x=> x.DeviceId)
+            .NotEmpty().WithMessage(Errors.General.ValueIsEmpty(nameof(VerifyNonceCommand.DeviceId)).Message);
+        RuleFor(x=> x.Nonce)
+            .NotEmpty()
+            .WithMessage(Errors.General.ValueIsEmpty(nameof(VerifyNonceCommand.Nonce)).Message)
+            .MaximumLength(44).WithMessage(Errors.General.ValueTooLarge(nameof(VerifyNonceCommand.Nonce), 44).Message);
     }
 }
