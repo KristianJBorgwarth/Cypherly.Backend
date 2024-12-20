b0d9a97439394fe18c44b8d522e9043b7a38b696
Kristian <fich2402@gmail.com>
2024-12-20 13:37:10 +0100

    unit test for cmdhandler, -validator and verifynonceservice

---
+++ Cypherly.Authentication.Application.Test.Unit/Setup/Helpers/Ed25519Helper.cs
@@ -0,0 +1,21 @@
+﻿using System.Text;
+using Org.BouncyCastle.Crypto.Parameters;
+using Org.BouncyCastle.Security;
+
+namespace Cypherly.Authentication.Application.Test.Unit.Setup.Helpers;
+
+public static class Ed25519Helper
+{
+    public static string SignNonce(string nonce, string privateKeyBase64)
+    {
+        var nonceBytes = Encoding.UTF8.GetBytes(nonce);
+        var privateKey = Convert.FromBase64String(privateKeyBase64);
+        var privateKeyParam = new Ed25519PrivateKeyParameters(privateKey, 0);
+
+        var signer = SignerUtilities.GetSigner("Ed25519");
+        signer.Init(true, privateKeyParam);
+        signer.BlockUpdate(nonceBytes, 0, nonceBytes.Length);
+        var signature = signer.GenerateSignature();
+        return Convert.ToBase64String(signature);
+    }
+}
