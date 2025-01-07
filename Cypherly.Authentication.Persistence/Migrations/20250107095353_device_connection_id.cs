117c9aa80fd0d16ff06280e0fe0ff9e27d828022
Kristian <89026749+KristianJBorgwarth@users.noreply.github.com>
2025-01-07 12:37:34 +0100

    Device ConnectionId(#47)

---
+++ Cypherly.Authentication.Persistence/Migrations/20250107095353_device_connection_id.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Cypherly.Authentication.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class device_connection_id : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "ConnectionId",
+                table: "Device",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ConnectionId",
+                table: "Device");
+        }
+    }
+}
