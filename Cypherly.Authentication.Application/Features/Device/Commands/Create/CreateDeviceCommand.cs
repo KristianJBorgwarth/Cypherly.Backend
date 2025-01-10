c20669ad47f9019fb58ea5da3782930fb8b2a26e
Kristian <fich2402@gmail.com>
2025-01-10 01:03:31 +0100

    setup to move device crt out of login

---
+++ Cypherly.Authentication.Application/Features/Device/Commands/Create/CreateDeviceCommand.cs
@@ -0,0 +1,13 @@
+﻿using Cypherly.Application.Abstractions;
+using Cypherly.Authentication.Domain.Enums;
+
+namespace Cypherly.Authentication.Application.Features.Device.Commands.Create;
+
+public sealed record CreateDeviceCommand : ICommand<CreateDeviceDto>
+{
+    public required Guid UserId { get; init; }
+    public required string DeviceAppVersion { get; init; }
+    public required DeviceType DeviceType { get; init; }
+    public required DevicePlatform DevicePlatform { get; init; }
+    public required string Base64DevicePublicKey { get; init; }
+}
