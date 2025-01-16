ed30844f23db0d01075cdef1db11c6a39f4e262c
Kristian <89026749+KristianJBorgwarth@users.noreply.github.com>
2025-01-16 14:39:32 +0100

    Feat: GetDevices (#50)
    * added Get Devices Endpoint
    
    * initial test
    
    * feat test
    
    ---------
    
    Co-authored-by: kristian <kj.borgwarth@outlook.com>

---
+++ Cypherly.Authentication.Application/Features/Device/Queries/GetDevices/GetDevicesQuery.cs
@@ -0,0 +1,8 @@
+﻿using Cypherly.Application.Abstractions;
+
+namespace Cypherly.Authentication.Application.Features.Device.Queries.GetDevices;
+
+public sealed record GetDevicesQuery : IQuery<GetDevicesDto>
+{
+    public required Guid UserId { get; init; }
+}
