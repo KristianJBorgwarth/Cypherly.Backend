f20957ba0dcd1599df6bcffdab8c9e4d51e39def
kristian <kj.borgwarth@outlook.com>
2025-01-17 12:14:19 +0100

    added logging to chatserver

--- Cypherly.ChatServer.API/Program.cs
+++ Cypherly.ChatServer.API/Program.cs
@@ -1,10 +1,35 @@
 using System.Reflection;
 using Cypherly.ChatServer.Application.Configuration;
+using Serilog;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+#region Configuration 
+
+var env = builder.Environment;
+
+var configuration = builder.Configuration;
+configuration.AddJsonFile("appsettings.json", false, true)
+    .AddEnvironmentVariables();
+
+if(env.IsDevelopment())
+{
+    configuration.AddJsonFile($"appsettings.{Environments.Development}.json", true, true);
+    configuration.AddUserSecrets(Assembly.GetExecutingAssembly(), true);
+}
+
+#endregion
+
+#region Logger
+
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(configuration)
+    .CreateLogger();
+
+builder.Host.UseSerilog();
+
+#endregion
 
 #region Application Layer
 
@@ -12,6 +37,8 @@ builder.Services.AddChatServerApplication(Assembly.Load("Cypherly.ChatServer.App
 
 #endregion
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
@@ -19,6 +46,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+Log.Information("Chat Server booted up");
 
 app.UseHttpsRedirection();
 
