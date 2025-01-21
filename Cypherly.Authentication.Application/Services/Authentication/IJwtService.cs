e7fb494596c5291215a33bcdd0c98555f0dfad66
Kristian <fich2402@gmail.com>
2025-01-21 14:40:42 +0100

    added new jwt method

--- Cypherly.Authentication.Application/Services/Authentication/IJwtService.cs
+++ Cypherly.Authentication.Application/Services/Authentication/IJwtService.cs
@@ -5,4 +5,5 @@ namespace Cypherly.Authentication.Application.Services.Authentication;
 public interface IJwtService
 {
     string GenerateToken(Guid UserId, string UserEmail, List<UserClaim> claims);
+    string GenerateToken2(Guid UserId, Guid DeviceId, List<UserClaim> userClaims);
 }
