8aa3d105409461682633c804344ae9101933b948
Kristian <fich2402@gmail.com>
2025-02-02 00:09:46 +0100

    backplane configuration

--- Cypherly.ChatServer.API/Hubs/MessageHub.cs
+++ Cypherly.ChatServer.API/Hubs/MessageHub.cs
@@ -4,5 +4,8 @@ namespace Cypherly.ChatServer.API.Hubs;
 
 public class MessageHub : Hub
 {
-
+    public async Task TestSendMessage(string message)
+    {
+        await Clients.All.SendAsync("ReceiveMessage", message);
+    }
 }
