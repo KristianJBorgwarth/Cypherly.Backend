803b820b815b992ff9ec071057b953e1a640ac7f
Kristian <89026749+KristianJBorgwarth@users.noreply.github.com>
2025-02-09 12:47:35 +0100

    Feature: Get pending friend requests (#61)
    * setup for query
    
    * implemented query, handler and query validator
    
    * added integration and unit test

---
+++ Cypherly.UserManagement.Application/Features/UserProfile/Queries/GetFriendRequests/GetFriendRequestsDto.cs
@@ -0,0 +1,20 @@
+﻿namespace Cypherly.UserManagement.Application.Features.UserProfile.Queries.GetFriendRequests;
+
+public class GetFriendRequestsDto
+{
+    public string Username { get; private init; } = null!;
+    public string UserTag { get;  private init; } = null!;
+    public string? DisplayName { get; private init; }
+
+    private GetFriendRequestsDto() { }
+
+    public static GetFriendRequestsDto MapFrom(string username, string userTag, string? displayName)
+    {
+        return new GetFriendRequestsDto
+        {
+            Username = username,
+            UserTag = userTag,
+            DisplayName = displayName,
+        };
+    }
+}
