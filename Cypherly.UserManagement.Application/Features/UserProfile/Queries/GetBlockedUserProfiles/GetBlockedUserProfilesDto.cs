01c3bc70b8eff5872666ae5811c18527c83b9763
Kristian <fich2402@gmail.com>
2025-02-09 12:55:59 +0100

    setup query, handler, validator, endpoint

---
+++ Cypherly.UserManagement.Application/Features/UserProfile/Queries/GetBlockedUserProfiles/GetBlockedUserProfilesDto.cs
@@ -0,0 +1,6 @@
+﻿namespace Cypherly.UserManagement.Application.Features.UserProfile.Queries.GetBlockedUserProfiles;
+
+public class GetBlockedUserProfilesDto
+{
+    
+}
---
+++ Cypherly.UserManagement.Application/Features/UserProfile/Queries/GetBlockedUserProfiles/GetBlockedUserProfilesQueryHandler.cs
@@ -0,0 +1,12 @@
+﻿using Cypherly.Application.Abstractions;
+using Cypherly.Domain.Common;
+
+namespace Cypherly.UserManagement.Application.Features.UserProfile.Queries.GetBlockedUserProfiles;
+
+public sealed class GetBlockedUserProfilesQueryHandler : IQueryHandler<GetBlockedUserProfilesQuery, List<GetBlockedUserProfilesDto>>
+{
+    public Task<Result<List<GetBlockedUserProfilesDto>>> Handle(GetBlockedUserProfilesQuery request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
