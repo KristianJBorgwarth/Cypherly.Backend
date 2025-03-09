08f5fe6013bd4296a4ba8547632ea4dd6cb6d544
Kristian <89026749+KristianJBorgwarth@users.noreply.github.com>
2025-03-09 19:40:18 +0100

    removed connectionfilter and placed config in configuration methods (#93)

---
+++ Cypherly.ChatServer.API/Configuration/ConnectionMultiPlexerConfiguration.cs
@@ -0,0 +1,37 @@
+﻿using Cypherly.ChatServer.Valkey.Configuration;
+using Microsoft.Extensions.Options;
+using Serilog;
+using StackExchange.Redis;
+
+namespace Cypherly.ChatServer.API.Configuration;
+
+public static class ConnectionMultiPlexerConfiguration
+{
+    public static IServiceCollection AddConnectionMultiplexer(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.Configure<ValkeySettings>(configuration.GetSection("Valkey"));
+
+        services.AddSingleton<IConnectionMultiplexer>(provider =>
+        {
+            var valkeySettings = provider.GetRequiredService<IOptions<ValkeySettings>>().Value;
+            var config = new ConfigurationOptions
+            {
+                AbortOnConnectFail = false,
+                EndPoints = { $"{valkeySettings.Host}:{valkeySettings.Port}" },
+                ChannelPrefix = "Cypherly.ChatServer.API_",
+            };
+            var multiplexer = ConnectionMultiplexer.Connect(config);
+
+            multiplexer.ConnectionFailed += (_, e) =>
+                Log.Warning("Connection to Valkey failed: {Exception}", e.Exception);
+
+            multiplexer.ConnectionRestored += (_, e) =>
+                Log.Information("Connection to Valkey restored");
+
+            Log.Information("Connected to Valkey");
+            return multiplexer;
+        });
+
+        return services;
+    }
+}
---
+++ Cypherly.ChatServer.API/Configuration/SignalRConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Cypherly.ChatServer.API.Handlers;
+using Cypherly.ChatServer.Application.Contracts;
+using StackExchange.Redis;
+
+namespace Cypherly.ChatServer.API.Configuration;
+
+public static class SignalRConfiguration
+{
+    public static IServiceCollection ConfigureSignalR(this IServiceCollection services)
+    {
+        services.AddSignalR()
+            .AddStackExchangeRedis(options =>
+            {;
+                options.ConnectionFactory = async _ =>
+                    await Task.FromResult(services.BuildServiceProvider()
+                        .GetRequiredService<IConnectionMultiplexer>());
+            });
+
+        services.AddScoped<IChangeEventNotifier, ChangeEventHandler>();
+        return services;
+    }
+}
